572dc822325a89c2a483b30b72b850a1316ea67e
Kasper Rustad <kj4sper@gmail.com>
2025-11-03 18:07:55 +0100

    Legger til ny webapp
    Legger til filene til `NRLWebApp`, den nye webapplikasjonen.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NRLWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
